abdef8bfa3992bb5a777aa7378a8552e7dadb74c
janet <janetngei18@gmail.com>
2023-03-11 00:23:05 +0300

    sqlite tables, migtarions, schema and database

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,72 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_10_200949) do
+  create_table "appointments", force: :cascade do |t|
+    t.integer "patient_id", null: false
+    t.integer "doctor_id", null: false
+    t.string "start_date"
+    t.string "end_date"
+    t.string "title"
+    t.string "location"
+    t.string "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
+  end
+
+  create_table "departments", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "title"
+    t.string "name"
+    t.string "bio"
+    t.integer "department_id", null: false
+    t.string "email"
+    t.string "password_digest"
+    t.string "image_url"
+    t.boolean "doc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["department_id"], name: "index_doctors_on_department_id"
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.string "age"
+    t.string "birthdate"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "doc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "results", force: :cascade do |t|
+    t.integer "WBC"
+    t.integer "RBC"
+    t.integer "haemogloblin"
+    t.integer "haematocrit"
+    t.integer "platelates"
+    t.integer "sodium"
+    t.integer "potassium"
+    t.integer "bun"
+    t.integer "calcium"
+    t.integer "patient_id", null: false
+    t.string "test_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["patient_id"], name: "index_results_on_patient_id"
+  end
+
+  add_foreign_key "appointments", "doctors"
+  add_foreign_key "appointments", "patients"
+  add_foreign_key "doctors", "departments"
+  add_foreign_key "results", "patients"
 end
