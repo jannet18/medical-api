378880dd0efe8b9a37d4430bc5d8424b7b4b15c2
janet <janetngei18@gmail.com>
2023-03-12 18:23:17 +0300

    models

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,16 @@
 class Doctor < ApplicationRecord
+    has_seure_password
+    belongs_to :department
+    has_many :appointments, dependent: :nullify
+    has_many :patients, through: :appointments
+
+    validates :email, presence: true, uniqueness: true
+    validates :name, presence: true
+    validates :permitted_email
+
+    def permitted_email
+        unless email.match?(/tigonimed.edu/)
+            errors.add(:permitted_emails, ":Must have a Tigoni Med email.")
+        end
+    end
 end
